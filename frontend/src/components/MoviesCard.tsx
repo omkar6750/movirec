f515a7f80855813982b23d7ce778705b13dd0bb7
Omkar Pawar <pawaromkar1654@gmail.com>
2025-11-26 15:00:50 +0530

    build error corrected

--- frontend/src/components/MoviesCard.tsx
+++ frontend/src/components/MoviesCard.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Heart, Plus, Check, Star } from "lucide-react";
