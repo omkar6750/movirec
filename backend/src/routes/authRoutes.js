290ca8e72c2a558eadcb3be1c3f459211b0971a0
Omkar Pawar <pawaromkar1654@gmail.com>
2025-11-26 13:03:42 +0530

    secure true

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -21,7 +21,7 @@ router.get('/auth/google/callback',
                 res.cookie('token', token, {
                         httpOnly: true,
                         sameSite: 'none',
-                        secure: process.env.NODE_ENV === 'production',
+                        secure: true,
                         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
                 });
 
